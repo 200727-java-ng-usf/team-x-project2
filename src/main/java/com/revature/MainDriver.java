4c42222d9bdaa3a254ef8cf174d672c94635aa70
Chris Yanoso <christopheryanoso@gmail.com>
2020-09-22 11:47:08 -0400

    hibernate connection established

--- src/main/java/com/revature/MainDriver.java
+++ src/main/java/com/revature/MainDriver.java
@@ -1,4 +1,12 @@
 package com.revature;
 
+import com.revature.util.HibernateUtil;
+import org.hibernate.Hibernate;
+import org.hibernate.SessionFactory;
+
 public class MainDriver {
+
+    public static void main(String[] args) {
+        System.out.println(HibernateUtil.getSessionFactory());
+    }
 }
