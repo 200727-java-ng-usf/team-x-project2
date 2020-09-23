1e1fc2708685efffce625ad437645658c10cb606
EPaetow <60556354+EPaetow@users.noreply.github.com>
2020-09-23 13:47:33 -0500

    exceptions package. invalid and authentication. repo package. working on authenticate in user service

--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -1,6 +1,40 @@
 package com.revature.services;
 
+import com.revature.exceptions.AuthenticationException;
+import com.revature.exceptions.InvalidRequestException;
+import com.revature.models.User;
+import com.revature.repos.UserRepo;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 
+    //login
+    //get info
+    //register
+    //update
+    //delete
+    //update password
+    //update home only
 
+    private UserRepo userRepo = new UserRepo();
+
+    //no args constructor
+    public UserService() {
+        System.out.println("UserService no-args constructor invoked!");
+    }
+
+    //validate username and password
+    public User authenticate(String username, String password) {
+        if (username == null || username.trim().equals("") || password == null || password.trim().equals("")) {
+            throw new InvalidRequestException("Invalid credential values provided!");
+        }
+
+        return userRepo.findUserByCredentials(username, password)
+                .orElseThrow(AuthenticationException::new);
+
+    }
 }
+
+
+
