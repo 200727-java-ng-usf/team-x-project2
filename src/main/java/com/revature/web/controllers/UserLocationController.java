16a9bf56ef8e854425d47122b60f5ff88acf51dd
Chris Yanoso <christopheryanoso@gmail.com>
2020-09-29 10:38:30 -0400

    added UserLocation structure

---
+++ src/main/java/com/revature/web/controllers/UserLocationController.java
@@ -0,0 +1,18 @@
+package com.revature.web.controllers;
+
+import com.revature.services.UserLocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/location")
+public class UserLocationController {
+
+    private UserLocationService userLocationService;
+
+    @Autowired
+    public UserLocationController(UserLocationService userLocationService) {
+        this.userLocationService = userLocationService;
+    }
+}
