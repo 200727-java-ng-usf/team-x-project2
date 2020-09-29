7ae0e96637e67c427b6381632a567009bfe8787e
Chris Yanoso <christopheryanoso@gmail.com>
2020-09-29 19:08:52 -0400

    added findAllFacoriteLocations for a User

--- src/main/java/com/revature/web/controllers/UserLocationController.java
+++ src/main/java/com/revature/web/controllers/UserLocationController.java
@@ -1,10 +1,16 @@
 package com.revature.web.controllers;
 
+import com.revature.dtos.Principal;
+import com.revature.models.Location;
 import com.revature.services.UserLocationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/user/location")
@@ -17,5 +23,9 @@ public class UserLocationController {
         this.userLocationService = userLocationService;
     }
 
-
+    @GetMapping(value="/favorites", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Set<Location> getAllFavoriteLocations(HttpServletRequest request){
+        Principal principal = (Principal) request.getSession().getAttribute("principal");
+        return userLocationService.findAllFavoriteLocations(principal.getUserId());
+    }
 }
