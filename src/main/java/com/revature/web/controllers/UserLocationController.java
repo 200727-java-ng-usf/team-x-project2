0115b90f085d3edc915cbda65db83130f35eb539
Chris Yanoso <christopheryanoso@gmail.com>
2020-09-29 10:51:45 -0400

    fixed unauthorized error

--- src/main/java/com/revature/web/controllers/UserLocationController.java
+++ src/main/java/com/revature/web/controllers/UserLocationController.java
@@ -2,6 +2,7 @@ package com.revature.web.controllers;
 
 import com.revature.services.UserLocationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +16,6 @@ public class UserLocationController {
     public UserLocationController(UserLocationService userLocationService) {
         this.userLocationService = userLocationService;
     }
+
+
 }
