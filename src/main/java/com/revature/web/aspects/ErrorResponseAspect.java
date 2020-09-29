b1794e9dbd7f796e3fc072c4f50ab5f1b46cfc35
Chris Yanoso <christopheryanoso@gmail.com>
2020-09-28 22:24:34 -0400

    logging aspect added, finished errorResponse aspect

--- src/main/java/com/revature/web/aspects/ErrorResponseAspect.java
+++ src/main/java/com/revature/web/aspects/ErrorResponseAspect.java
@@ -3,6 +3,9 @@ package com.revature.web.aspects;
 
 import com.revature.dtos.ErrorResponse;
 import com.revature.exceptions.AuthenticationException;
+import com.revature.exceptions.FailedTransactionException;
+import com.revature.exceptions.InvalidRequestException;
+import com.revature.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,4 +22,21 @@ public class ErrorResponseAspect {
         return new ErrorResponse(401, ae.getMessage());
     }
 
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleFailedTransactionException(FailedTransactionException fte){
+        return new ErrorResponse(500, fte.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleInvalidRequestException(InvalidRequestException ire){
+        return new ErrorResponse(400, ire.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleResourceNotFoundException (ResourceNotFoundException rnfe){
+        return new ErrorResponse(404, rnfe.getMessage());
+    }
 }
